1c6399ad1e0963c14e4da5304937a086c21d9665
kaitohuy <huynguyendoan0305@gmail.com>
2026-03-30 09:27:00 +0700

    add admin component

---
+++ src/app/features/admin/users/users.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { UsersComponent } from './users.component';
+
+const routes: Routes = [
+  { path: '', component: UsersComponent }
+];
+
+@NgModule({
+  imports: [UsersComponent, RouterModule.forChild(routes)]
+})
+export class UsersModule {}
