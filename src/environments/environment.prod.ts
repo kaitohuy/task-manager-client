a0f9e44072161628f72068f851617c44631fa0c9
kaitohuy <huynguyendoan0305@gmail.com>
2026-04-16 15:23:45 +0700

    fix enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080', // TODO: Cập nhật URL Backend Production tại đây
-  wsUrl: 'http://localhost:8080/ws' // TODO: Cập nhật URL WebSocket Production tại đây
+  apiUrl: 'https://taskmanagerdeha.onrender.com', // TODO: Cập nhật URL Backend Production tại đây
+  wsUrl: 'https://taskmanagerdeha.onrender.com/ws' // TODO: Cập nhật URL WebSocket Production tại đây
 };
